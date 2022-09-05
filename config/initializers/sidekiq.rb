b820de8eb8b63d661ab96d539603191118121e66
tjs <tejas.g@simformsolutions.com>
2022-09-05 19:47:30 +0530

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
-redis 'sidekiq/server'
+redis "sidekiq/server"
 
-if Rails.environment.development?
-  Sidekiq.redis = Redis.new(host: 'localhost', port: '6379')
-else
-  uri = URI.parse(ENV['REDIS_URL'])
-  REDIS = SIDEKIQ.new(host: uri.host, port:uri.port, password: uri.password)
-  Sidekiq.redis = REDIS
-end
+# if Rails.environment.development?
+#   Sidekiq.redis = Redis.new(host: 'localhost', port: '6379')
+# else
+#   uri = URI.parse(ENV['REDIS_URL'])
+#   REDIS = SIDEKIQ.new(host: uri.host, port:uri.port, password: uri.password)
+#   Sidekiq.redis = REDIS
+# end
 # Sidekiq.configure_server do |config|
-#  config.redis = { url: 'redis://localhost:6379/0'}   
+#  config.redis = { url: 'redis://localhost:6379/0'}
 # end
 
 # Sidekiq.configure_client do |config|
